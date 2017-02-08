74c179c01041c3d5b30677663e7223a2e7d2733e
Andrea Mola <mola.andrea@gmail.com>
2017-02-08 17:53:03 +0100

    implemented modifications required by Nicola for the last pull request

--- include/computational_domain.h
+++ include/computational_domain.h
@@ -196,6 +196,7 @@ public:
   /// Strings identifying the input grid name and format
   std::string input_grid_name;
   std::string input_grid_format;
+  std::string input_cad_path;
 
   /// the material ID numbers in the mesh
   /// input file, for the dirichlet_nodes

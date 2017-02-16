e6126729ceef8012bad4d75f0099a8e83f25e3f7
Andrea Mola <mola.andrea@gmail.com>
2017-02-16 16:26:05 +0100

    fixed problem in the test for the cad import and mesh refinement procedure

--- tests/cad_import_and_curvature_refinement.cc
+++ tests/cad_import_and_curvature_refinement.cc
@@ -15,14 +15,13 @@
 #include "tests.h"
 #include "computational_domain.h"
 
-int main ()
+int main (int argc, char **argv)
 {
-  initlog(); 
-  ParameterAcceptor::initialize(SOURCE_DIR "/parameter_cad_import_and_curvature_refinement.prm","used.prm");
+  Utilities::MPI::MPI_InitFinalize mpi_initialization(argc, argv, 1);
+  //initlog(); 
   MPI_Comm mpi_communicator(MPI_COMM_WORLD);
   ComputationalDomain<3> computational_domain(mpi_communicator);
   ParameterAcceptor::initialize(SOURCE_DIR "/parameter_cad_import_and_curvature_refinement.prm","used.prm");
   computational_domain.read_domain();
   computational_domain.refine_and_resize(computational_domain.n_cycles);
-  deallog << 0 << std::endl;
 }

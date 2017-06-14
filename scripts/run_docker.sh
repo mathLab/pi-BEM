e450ab8dc2c25935e047eae56af604cd8589708b
Nicola Giuliani <ngiuliani@sissa.it>
2017-06-14 15:38:18 +0200

    minors

--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-IMG=`grep image ../.gitlab-ci.yml | awk '{print $2}'` 
+IMG=`grep image .travis.yml | awk '{print $2}'` 
 echo $IMG
 
-CMD=`grep -e '- ' ../.gitlab-ci.yml | sed 's/- //'`
+CMD=`grep -e '- ' .travis.yml | sed 's/- //'`
 docker pull $IMG 
 docker run -v `pwd`/..:/builds/studenti/pi-BEM/ $IMG /bin/sh -c "$CMD" 

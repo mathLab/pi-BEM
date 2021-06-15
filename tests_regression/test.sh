2c88ca07d1e254c832942e7a8e54f3cad6f98f28
lfabris <lfabris@sissa.it>
2021-06-15 18:01:35 +0200

    started adding nthreads in logs etc

--- tests_regression/test.sh
+++ tests_regression/test.sh
@@ -17,7 +17,8 @@ cmake --build . -j4
 #-- plain scalar phi, v2; different function
 #-- basic 2-component phi, (v1, v2)
 
-tbb_nthreads=1
+nthreads=1
+ncores_total=4
 
 echo "reference problems (np=1 fixed)"
 #generate the baseline - this should be skipped if ref* files are already present
@@ -30,13 +31,13 @@ do
         cp ${startdir}/ref_parameters_bem_3_f${func}_${mode}.prm ./parameters_bem_3.prm
 
         #references should be for np=1; np>1 are regressions
-        for np in 1
+        for nprocs in 1
         do
             if [ ! -f "${startdir}/ref_f${func}_${mode}.log" ]
             then
-                echo "reference problem ${func} - np ${np} - ${mode}"
-                export OMP_NUM_THREADS=${tbb_nthreads}
-                perf stat --detailed mpirun --np ${np} --map-by node:PE=1 --bind-to none bem_fma_3d ${tbb_nthreads} > log 2>&1
+                echo "reference problem ${func} - np ${nprocs} - nthreads ${nthreads} - solver ${mode}"
+                export OMP_NUM_THREADS=${nthreads}
+                perf stat --detailed mpirun --np ${nprocs} --map-by node:PE=1 --bind-to none bem_fma_3d ${nthreads} > log 2>&1
                 
                 result=${?}
     
@@ -46,7 +47,7 @@ do
                     do
                         cp ./${fn}.vtu ${startdir}/ref_${fn}_f${func}_${mode}.vtu
                         
-                        echo "reg_np${np}_${fn}_f${func}_${mode}.vtu"
+                        echo "reg_np${nprocs}_${fn}_f${func}_${mode}.vtu"
                         meshio-info ${startdir}/ref_${fn}_f${func}_${mode}.vtu
                     done
                     
@@ -54,7 +55,7 @@ do
                     cp log ${startdir}/ref_f${func}_${mode}.log
                     cat log >> ${startdir}/ref_f${func}_${mode}_log.log
                 else
-                    echo "error during reference problem ${func} - np ${np} - ${mode}"
+                    echo "error during reference problem ${func} - np ${nprocs} - nthreads ${nthreads} - solver ${mode}"
                 fi
                 echo
                 sleep ${cooldown}
@@ -63,7 +64,7 @@ do
     done
 done
 
-exit 0
+#exit 0
 
 #solve the simple problem instances with multiple mpi procs
 #logs are saved as reg_*
@@ -75,11 +76,11 @@ do
         cp ${startdir}/ref_parameters_bem_3_f${func}_${mode}.prm ./parameters_bem_3.prm
 
         #references should be for np=1; np>1 are regressions
-        for np in 2 4
+        for nprocs in 2 4
         do 
-            echo "regression problem ${func} - np ${np} - ${mode}"
-            export OMP_NUM_THREADS=${tbb_nthreads}
-            perf stat --detailed mpirun --np ${np} --map-by node:PE=1 --bind-to none bem_fma_3d ${tbb_nthreads} > log 2>&1
+            echo "regression problem ${func} - np ${nprocs} - nthreads ${nthreads} - solver ${mode}"
+            export OMP_NUM_THREADS=${nthreads}
+            perf stat --detailed mpirun --np ${nprocs} --map-by node:PE=1 --bind-to none bem_fma_3d ${nthreads} > log 2>&1
 
             result=${?}
 
@@ -87,17 +88,17 @@ do
             then
                 for fn in result_scalar_results result_vector_results #scalar_error vector_error
                 do
-                    cp ./${fn}.vtu ${startdir}/reg_np${np}_${fn}_f${func}_${mode}.vtu
+                    cp ./${fn}.vtu ${startdir}/reg_np${nprocs}_nt${nthreads}_${fn}_f${func}_${mode}.vtu
                     
-                    echo "reg_np${np}_${fn}_f${func}_${mode}.vtu"
-                    meshio-info ${startdir}/reg_np${np}_${fn}_f${func}_${mode}.vtu
+                    echo "reg_np${nprocs}_nt${nthreads}_${fn}_f${func}_${mode}.vtu"
+                    meshio-info ${startdir}/reg_np${nprocs}_nt${nthreads}_${fn}_f${func}_${mode}.vtu
                 done
                 
                 #the first is for simple reading, the one appending is for python mega pandas dataframing
-                cp log ${startdir}/reg_np${np}_f${func}_${mode}.log
-                cat log >> ${startdir}/reg_np${np}_f${func}_${mode}_log.log
+                cp log ${startdir}/reg_np${nprocs}_nt${nthreads}_f${func}_${mode}.log
+                cat log >> ${startdir}/reg_np${nprocs}_nt${nthreads}_f${func}_${mode}_log.log
             else
-                echo "error during regression problem ${func} - np ${np} - ${mode}"
+                echo "error during regression problem ${func} - np ${nprocs} - nthreads ${nthreads} - solver ${mode}"
             fi
             echo
             sleep ${cooldown}
@@ -112,11 +113,11 @@ for mode in direct fma
 do
     cp ${startdir}/ref_parameters_bem_3_complex_${mode}.prm ./parameters_bem_3.prm
 
-    for np in 1 2 4
+    for nprocs in 1 2 4
     do 
-        echo "complex problem - np ${np} - ${mode}"
-        export OMP_NUM_THREADS=${tbb_nthreads}
-	    perf stat --detailed mpirun --np ${np} --map-by node:PE=1 --bind-to none bem_fma_3d ${tbb_nthreads} > log 2>&1
+        echo "complex problem - np ${nprocs} - nthreads ${nthreads} - solver ${mode}"
+        export OMP_NUM_THREADS=${nthreads}
+	    perf stat --detailed mpirun --np ${nprocs} --map-by node:PE=1 --bind-to none bem_fma_3d ${nthreads} > log 2>&1
 
         result=${?}
 
@@ -125,10 +126,10 @@ do
             #first component has no suffix on filename
             for fn in result_scalar_results result_vector_results #scalar_error vector_error
             do
-                cp ./${fn}.vtu ${startdir}/reg_np${np}_${fn}_complex_f1_${mode}.vtu
+                cp ./${fn}.vtu ${startdir}/reg_np${nprocs}_nt${nthreads}_${fn}_complex_f1_${mode}.vtu
                 
-                echo "reg_np${np}_${fn}_complex_f1.vtu"
-                meshio-info ${startdir}/reg_np${np}_${fn}_complex_f1_${mode}.vtu
+                echo "reg_np${nprocs}_${fn}_complex_f1.vtu"
+                meshio-info ${startdir}/reg_np${nprocs}_nt${nthreads}_${fn}_complex_f1_${mode}.vtu
             done
             #do the remaining comps
             for func in 2
@@ -136,18 +137,18 @@ do
                 for fn in result_${func}_scalar_results result_${func}_vector_results #scalar_${func}_error vector_${func}_error
                 do
                     fn_redux="${fn/_${func}/}"
-                    cp ./${fn}.vtu ${startdir}/reg_np${np}_${fn_redux}_complex_f${func}_${mode}.vtu
+                    cp ./${fn}.vtu ${startdir}/reg_np${nprocs}_nt${nthreads}_${fn_redux}_complex_f${func}_${mode}.vtu
                     
-                    echo "reg_np${np}_${fn_redux}_complex_f${func}.vtu"
-                    meshio-info ${startdir}/reg_np${np}_${fn_redux}_complex_f${func}_${mode}.vtu
+                    echo "reg_np${nprocs}_nt${nthreads}_${fn_redux}_complex_f${func}.vtu"
+                    meshio-info ${startdir}/reg_np${nprocs}_nt${nthreads}_${fn_redux}_complex_f${func}_${mode}.vtu
                 done
             done
             
             #the first is for simple reading, the one appending is for python mega pandas dataframing
-            cp log ${startdir}/reg_np${np}_complex_${mode}.log
-            cat log >> ${startdir}/reg_np${np}_complex_${mode}_log.log
+            cp log ${startdir}/reg_np${nprocs}_nt${nthreads}_complex_${mode}.log
+            cat log >> ${startdir}/reg_np${nprocs}_nt${nthreads}_complex_${mode}_log.log
         else
-            echo "error during complex problem - np ${np} - ${mode}"
+            echo "error during complex problem - np ${nprocs} - nthreads ${nthreads} - solver ${mode}"
         fi
         echo
         sleep ${cooldown}
--- tests_regression/test_compares.sh
+++ tests_regression/test_compares.sh
@@ -6,12 +6,18 @@ for mode in direct fma
 do
     for func in 1 2
     do
-        for np in 2 4
+        for nprocs in 2 4
         do
-            for type in result_scalar_results result_vector_results #scalar_error vector_error
+            for nthreads in 1 2 4
             do
-                python mesh_compare.py ref_${type}_f${func}_${mode}.vtu reg_np${np}_${type}_f${func}_${mode}.vtu ${atol}
-                echo
+                for type in result_scalar_results result_vector_results #scalar_error vector_error
+                do
+                    if [ -f reg_np${nprocs}_nt${nthreads}_${type}_f${func}_${mode}.vtu ]
+                    then
+                        python mesh_compare.py ref_${type}_f${func}_${mode}.vtu reg_np${nprocs}_nt${nthreads}_${type}_f${func}_${mode}.vtu ${atol}
+                        echo
+                    fi
+                done
             done
         done
     done
@@ -22,12 +28,18 @@ for mode in direct fma
 do
     for func in 1 2
     do
-        for np in 1 2 4
+        for nprocs in 1 2 4
         do
-            for type in result_scalar_results #result_vector_results #scalar_error vector_error
+            for nthreads in 1 2 4
             do
-                python mesh_compare.py ref_${type}_f${func}_${mode}.vtu reg_np${np}_${type}_complex_f${func}_${mode}.vtu ${atol}
-                echo
+                for type in result_scalar_results #result_vector_results #scalar_error vector_error
+                do
+                    if [ -f reg_np${nprocs}_nt${nthreads}_${type}_complex_f${func}_${mode}.vtu ]
+                    then
+                        python mesh_compare.py ref_${type}_f${func}_${mode}.vtu reg_np${nprocs}_nt${nthreads}_${type}_complex_f${func}_${mode}.vtu ${atol}
+                        echo
+                    fi
+                done
             done
         done
     done
